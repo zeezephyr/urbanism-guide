1bbc8064ac1e0e594c2263d5b4dd6445c2459120
me <>
2023-11-07 16:54:30 -0600

    remove default extractor

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
     content: ["./hugo_stats.json"],
-    defaultExtractor: (content) => {
-        const els = JSON.parse(content).htmlElements;
-        return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-    },
+    // defaultExtractor: (content) => {
+    //     const els = JSON.parse(content).htmlElements;
+    //     return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+    // },
     safelist: [],
   });
   
